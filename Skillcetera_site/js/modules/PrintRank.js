1226793c4a804a76220ed0fe8f4a0974606d0522
Do Tien <105923398+NT-Tien@users.noreply.github.com>
2023-01-15 22:09:12 +0700

    Update PrintRank.js

--- Skillcetera_site/js/modules/PrintRank.js
+++ Skillcetera_site/js/modules/PrintRank.js
@@ -2,9 +2,9 @@ export function getRank(mark){
     var print = document.getElementById('rank');
     var str = '';
     if (mark >= 8) {
-        str = '<br><br><h1>Excellent member</h1>';
+        str = '<br><br><h1>Excellent member✨</h1>';
     } else if (mark >= 4 && mark < 8) {
-        str = '<br><br><h1>Good member</h1>';
+        str = '<br><br><h1>Good member 😍</h1>';
     } 
     print.innerHTML = str;
 }
